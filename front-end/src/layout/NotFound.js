9d4f2ee31cead7e5861f3963708da8f82addd9ec
Anthony Aviles <avilesanthony0617@gmail.com>
2021-08-08 08:44:08 -0700

    test commit

--- front-end/src/layout/NotFound.js
+++ front-end/src/layout/NotFound.js
@@ -3,13 +3,13 @@ import React from "react";
 /**
  * Defines the "Not Found" page that is displayed for any unmatched route.
  *
- * You will not need to make changes to this file.
+ * You will not need to    make changes to this file.
  *
  * @returns {JSX.Element}
  */
 function NotFound() {
   return (
-    <div className="NotFound">
+    <div className='NotFound'>
       <h1>Not Found</h1>
     </div>
   );
