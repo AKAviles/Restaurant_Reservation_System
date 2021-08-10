9637585a5b22159d43bef90af6446027740320ef
Anthony Aviles <avilesanthony0617@gmail.com>
2021-08-09 20:31:28 -0700

    additional deployment changes

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,6 +3,20 @@ const { PORT = 5000 } = process.env;
 const app = require("./app");
 const knex = require("./db/connection");
 
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 knex.migrate
   .latest()
   .then((migrations) => {
@@ -16,4 +30,11 @@ knex.migrate
 
 function listener() {
   console.log(`Listening on Port ${PORT}!`);
+}
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "front-end/build")));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "front-end/build", "index.html"));
+  });
 }
